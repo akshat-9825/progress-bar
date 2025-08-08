db30dc3dbb1596b6f02827a22d2619e9d30ae0e8
akshat-9825 <akshat10dwivedi@gmail.com>
2025-08-08 10:21:36 +0530

    dynamic progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import ProgressBar from "./components/progress-bar";
 
 function App() {
-  return <ProgressBar percentage={50} />;
+  const [percentage, setPercentage] = useState(50);
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 h-screen">
+      <div>Progress bar value: {percentage}</div>
+      <input
+        type="range"
+        min={0}
+        max={100}
+        value={percentage}
+        onChange={(e) => setPercentage(Number(e.target.value))}
+      />
+      <ProgressBar percentage={percentage} />
+    </div>
+  );
 }
 
 export default App;
